5dee25f41017fb14a79d14c0bc3346266254ed61
Gilmar Araujo <du.correia.ga@gmail.com>
2020-05-16 01:23:21 +0100

    Altered to input numbers and range

--- armstrong_number.py
+++ armstrong_number.py
@@ -1,27 +1,39 @@
-numeros= [1,5,3]
-length = len(numeros)
-valores = []
-valores_str = ""
-s = 0
-	
-#Colocando os valores ^ ao length dentro de um array
-for i in range(length):
-    valores.append((numeros[i] ** length))
-    #print(valores)
+def verifica_numero(entrada):
+    for c in range(entrada):
+        #numeros= input("Digite o numero a ser verificado:")
+        numeros = str(entrada)
+        length = len(numeros)
+        valores = []
+        valores_str = ""
+        s = 0
+            
+        #Colocando os valores ^ ao length dentro de um array
+        for i in range(length):
+            valores.append((int(numeros[i]) ** length))
+            #print(valores)
 
-#Somando valores 
-for i in range (len(valores)):
-    s = valores[i] + s
-    #print(s)
+        #Somando valores 
+        for i in range (len(valores)):
+            s = valores[i] + s
+            #print(s)
 
-# Convertendo os valores do array incial em String
-for i in range(len(numeros)):
-    valores_str =  valores_str + str(numeros[i]) 
-#print(valores_str)
+        # Convertendo os valores do array incial em String
+        for i in range(len(numeros)):
+            valores_str =  valores_str + str(numeros[i]) 
+        #print(valores_str)
 
-#Verificando se é um número de Armstrong
-if valores_str == str(s):
-   print("true")
+        #Verificando se é um número de Armstrong
+        if valores_str == str(s):
+           return True
+           
 
-else:
-    print("false")
+        else:
+            return False
+
+print(verifica_numero(4))
+
+for i in range(1000):
+    #print(verifica_numero(i))
+
+    if verifica_numero(i):
+        print(i)
